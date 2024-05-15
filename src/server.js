b3ce4ccbd7fbe56d6533fc5ccff909c99d3d9f8d
QueenCarine <queencarineh@gmail.com>
2024-05-15 08:10:14 +0200

    Update: swagger documentation

--- src/server.js
+++ src/server.js
@@ -1,27 +1,30 @@
-const dotenv = require ('dotenv');
-dotenv.config()
-const express = require('express');
-const userRoutes=require('../src/Routes/userRoutes.js')
-const swaggerUi=require('swagger-ui-express')
-const app = express()
-const mongoose = require ('mongoose');
-const cors=require('cors');
-const configuration = require('../src/config/index.js')
-const swagger = require('../src/docs/swagger.json');
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const userRoutes = require("../src/Routes/userRoutes.js");
+const swaggerUi = require("swagger-ui-express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const configuration = require("../src/config/index.js");
+const swagger = require("../src/docs/swagger.json");
+
 //middlewares
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 //routes
-app.use('/queen', swaggerUi.serve, swaggerUi.setup(swagger));
-app.use('/api/v1/auth',userRoutes)
+app.use("/queen", swaggerUi.serve, swaggerUi.setup(swagger));
+app.use("/api/v1/auth", userRoutes);
 
-mongoose.connect(configuration.mongoURI)
-.then(() => {
-    app.listen(configuration.port, ()=> {
-        console.log("listening on port "+configuration.port);
+
+mongoose
+  .connect(configuration.mongoURI)
+  .then(() => {
+    app.listen(configuration.port, () => {
+      console.log("listening on port " + configuration.port);
     });
-})
-.catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
-
+  });
+  
