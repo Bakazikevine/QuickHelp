3aa31ad4318a7cb9ff05a940da73d63a8c770f08
QueenCarine <queencarineh@gmail.com>
2024-05-21 11:38:25 +0200

    Errors

--- src/error/BadRequestError.js
+++ src/error/BadRequestError.js
@@ -1,23 +1,20 @@
-import CustomError from "./CustomError.js";
+const CustomError = require('./CustomError');
 
 /**
  * Custom error class for handling bad request errors.
  * @extends {CustomError} - Extends the CustomError class.
  */
-export class BadRequestError extends CustomError {
+class BadRequestError extends CustomError {
     /**
      * Constructor for the BadRequestError class.
      * @param {string} message - The error message to be displayed.
      */
     constructor(message) {
-        /**
-         * Call the superclass constructor with the provided message.
-         * @param {string} message - The error message to be displayed
-         */
+        // Call the superclass constructor with the provided message.
         super(message);
-        /**
-         * Set the HTTP status code for this error to 400.
-         */
+        // Set the HTTP status code for this error to 400.
         this.statusCode = 400;
     }
 }
+
+module.exports = BadRequestError;
