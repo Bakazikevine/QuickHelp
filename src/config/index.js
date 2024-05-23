dc0f693786bed0f708b7d660bee0a2f392106197
QueenCarine <queencarineh@gmail.com>
2024-05-23 11:56:01 +0200

    Booking

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 const dotenv =  require ("dotenv");
+const twilio = require('twilio');
 dotenv.config()
 const configs = {
     port: process.env.PORT ,
     mongoURI: process.env.MONGODB_URI ,
-    JWT_SECRET: process.env.SECRET 
+    JWT_SECRET: process.env.SECRET ,
+     
 }
 
 module.exports= configs;
